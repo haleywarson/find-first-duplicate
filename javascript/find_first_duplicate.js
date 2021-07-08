b84e4dd1ce0c97d8931ef724838379d812e586d6
Haley Warson <haleywarson@gmail.com>
2021-07-08 14:34:05 -0600

    Complete

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let uniques = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (uniques.includes(arr[i])) {
+      return arr[i];
+    }
+    uniques.push(arr[i]);
+  }
+
+  return -1;
 }
 
 if (require.main === module) {
